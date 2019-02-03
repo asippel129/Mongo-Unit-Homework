a092cc4d6f04f272a091cb82094c5a756b1840f9
Anna Sippel <asippel129@gmail.com>
2019-02-02 17:22:26 -0700

    FINALLY got thru the routes

---
+++ controllers/notes.js
@@ -0,0 +1,32 @@
+//bringing in our scrape script and makeDate scripts
+var Note = require("../scripts/Note");
+var makeDate = require("../scripts/date");
+
+module.exports = {
+    //get function to run all the notes associated w the articles
+    get: function(data, cb) {
+        Note.find({
+            _headlineId: data._id
+        }, cb);
+    },
+    save: function(data, cb) {
+        var newNote= {
+            _headlineId: data._id,
+            date: makeDate(),
+            noteText: data.noteText
+        };
+        Note.create(newNote, function(err, doc) {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log(doc);
+                cb(doc);
+            }
+        });
+    },
+    delete: function(data, cb) {
+        Note.remove({
+            _id: data._id
+        }, cb);
+    }
+};
